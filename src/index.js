9c2247481ee9377631ea59c879f4499a74f2c197
phil-sorenson <phillip.sorenson31@gmail.com>
2023-02-11 22:30:23 -0600

    Added Footer & Navbar to components

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter as Router } from "react-router-dom";
 
-
-
 ReactDOM.render(
   <React.StrictMode>
     <Router>
